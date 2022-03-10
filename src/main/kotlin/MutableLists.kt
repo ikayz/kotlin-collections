02fe9e91569861d88fa69a3e5883480ba7e3f291
Isaac Miti <ikayz@live.com>
2022-03-10 09:52:51 +0200

    Add more pasta to the list

--- src/main/kotlin/MutableLists.kt
+++ src/main/kotlin/MutableLists.kt
@@ -1,3 +1,13 @@
 fun main() {
+    val entrees = mutableListOf<String>()
+    println("Entrees: $entrees")
 
+    println("Add noodles: ${entrees.add("noodles")}")
+    println("Entrees: $entrees")
+    println("Add spaghetti: ${entrees.add("spaghetti")}")
+    println("Entrees: $entrees")
+
+    val pasta = listOf("ravioli", "lasagna", "fettuccine")
+    println("Add more pasta: {${entrees.addAll(pasta)}}")
+    println("Entrees: $entrees")
 }
