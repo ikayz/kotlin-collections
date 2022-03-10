1f7ba2645f949d49d8d6560f718704dcb1771657
Isaac Miti <ikayz@live.com>
2022-03-10 10:04:39 +0200

    Try out more mutable list methods

--- src/main/kotlin/MutableLists.kt
+++ src/main/kotlin/MutableLists.kt
@@ -2,12 +2,25 @@ fun main() {
     val entrees = mutableListOf<String>()
     println("Entrees: $entrees")
 
+    // Add single pasta items
     println("Add noodles: ${entrees.add("noodles")}")
     println("Entrees: $entrees")
     println("Add spaghetti: ${entrees.add("spaghetti")}")
     println("Entrees: $entrees")
 
+    // Add multiple pasta items
     val pasta = listOf("ravioli", "lasagna", "fettuccine")
     println("Add more pasta: {${entrees.addAll(pasta)}}")
+    println("Entrees: $entrees")
+
+    // Remove by element
+    println("Remove spaghetti: ${entrees.remove("spaghetti")}")
+    println("Entrees: $entrees")
+
+    // Remove by index
+    println("Remove noodles by index: ${entrees.removeAt(0)}")
+    println("Entrees: $entrees")
+
+    entrees.clear()
     println("Entrees: $entrees")
 }
