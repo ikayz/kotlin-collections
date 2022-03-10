05a205d3871d276c6e2b1ea2d08cc51e8ae44ec3
Isaac Miti <ikayz@live.com>
2022-03-10 10:49:27 +0200

    Override the vegetables and noodles class

--- src/main/kotlin/Practice.kt
+++ src/main/kotlin/Practice.kt
@@ -1,3 +1,20 @@
-fun main() {
+open class Item(val name: String, val price: Int)
 
+class Noodles : Item("Noodles", 10) {
+    override fun toString(): String {
+        return name
+    }
+}
+
+class Vegetables : Item("Vegetables", 5) {
+    override fun toString(): String {
+        return name
+    }
+}
+
+fun main() {
+    val noodles = Noodles()
+    val vegetables = Vegetables()
+    println(noodles)
+    println(vegetables)
 }
