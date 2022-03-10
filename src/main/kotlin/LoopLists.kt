50cd1aa79bf60278528fe4afd2b9078568624f23
Isaac Miti <ikayz@live.com>
2022-03-10 10:38:13 +0200

    Add for loop and loop through all the elements

--- src/main/kotlin/LoopLists.kt
+++ src/main/kotlin/LoopLists.kt
@@ -3,10 +3,19 @@ fun main() {
     var totalGuests = 0
     var index = 0
 
+    // While loop
     while (index < guestsPerFamily.size) {
         totalGuests += guestsPerFamily[index]
         index++
     }
 
     println("Total Guest Count: $totalGuests")
+
+    // For loop
+    val names = listOf("James", "John", "Wendy", "Belle")
+
+    for (name in names) {
+        println("$name - Number of characters ${name.length}")
+    }
+
 }
