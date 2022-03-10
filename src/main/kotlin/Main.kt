8b66c8a329a43dc25d37f5a7930aec340d71565f
Isaac Miti <ikayz@live.com>
2022-03-10 09:35:55 +0200

    Add the elements contained

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    val numbers = listOf(1, 2, 3, 4, 5, 6)
+    println("List: $numbers")
+    println("Size: ${numbers.size}")
+
+    // Access elements in a list
+    println("First element: ${numbers[0]}")
+    println("Second element: ${numbers[1]}")
+    println("Last index: ${numbers.size - 1}")
+    println("Last element: ${numbers[numbers.size - 1]}")
+    println("First: ${numbers.first()}")
+    println("Last: ${numbers.last()}")
+
+    // Check if the list contains elements using the contain method
+    println("Contains 4: ${numbers.contains(4)}")
+    println("Contains 7: ${numbers.contains(8)}")
+}
