6ff74c5271a0c4f4b1da0f1297b80989859c9535
Isaac Miti <ikayz@live.com>
2022-04-04 21:42:22 +0200

    Change code to return random word

--- src/main/kotlin/WordsList.kt
+++ src/main/kotlin/WordsList.kt
@@ -1,8 +1,9 @@
 fun main() {
     val words = listOf("about", "acute", "awesome", "balloon", "best", "brief", "class", "coffee", "creative")
 
-    val filteredWords = words.filter { it.startsWith("b", ignoreCase = true) }
+    val filteredWords = words.filter { it.startsWith("c", ignoreCase = true) }
         .shuffled()
-        .take(2)
+        .take(1)
+        .sorted()
     println(filteredWords)
 }
