6222e774ad720dd678f0618e25f921e9afca586a
Isaac Miti <ikayz@live.com>
2022-04-04 21:37:12 +0200

    Use startsWith to filter out a string

--- src/main/kotlin/WordsList.kt
+++ src/main/kotlin/WordsList.kt
@@ -1,3 +1,7 @@
 fun main() {
     val words = listOf("about", "acute", "awesome", "balloon", "best", "brief", "class", "coffee", "creative")
+
+    val filteredWords = words.filter { it.startsWith("c", ignoreCase = true) }
+        .shuffled()
+    println(filteredWords)
 }
